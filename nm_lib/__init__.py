d993a82625b87fb412f7adbd17489feb3304e922
Gaute Holen <gauteholen@Gautes-MBP-2.domain_not_set.invalid>
2023-06-08 18:38:31 +0200

    more import / req sheningans

--- nm_lib/__init__.py
+++ nm_lib/__init__.py
@@ -5,5 +5,5 @@ from .animation import *
 from .hydro import evolve_hydro, step_euler_diffusive
 from .Multispecies import Multispecies
 from .init_setup import *
-from analytical import *
-from colisions import CrossSection
+from .analytical import *
+from .colisions import CrossSection
